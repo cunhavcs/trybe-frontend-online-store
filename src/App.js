a9333c4b7b3b9db9bd0831f5ebba0d333de18069
cunhavcs <cunha.vcs@gmail.com>
2023-01-20 13:44:34 -0300

    Requisito 10 concluído

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ShoppingCart from './components/ShoppingCart';
 import SearchField from './components/SearchField';
 import ProductDetail from './components/ProductDetail';
@@ -8,13 +8,11 @@ import ProductDetail from './components/ProductDetail';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ SearchField } />
-          <Route path="/cart" component={ ShoppingCart } />
-          <Route path="/productdetail/:id" component={ ProductDetail } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ SearchField } />
+        <Route path="/cart" component={ ShoppingCart } />
+        <Route path="/productdetail/:id" component={ ProductDetail } />
+      </Switch>
     );
   }
 }
